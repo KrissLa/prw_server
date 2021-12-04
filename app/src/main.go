0621adc905e67d4706ba1498119425f6c9a9cbd0
Vitaliy Mishkov <vitaliy.mishkov@pm.by>
2021-12-05 02:55:07 +0300

    config

--- app/src/main.go
+++ app/src/main.go
@@ -5,19 +5,28 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/ilyakaznacheev/cleanenv"
 
+	"prw_server/app/config"
 	"prw_server/app/pkg/handlers"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 	handler := handlers.NewHandler()
 
 	router := chi.NewRouter()
 
 	router.Get("/hello", handler.Hello)
 
+	path := cfg.Host + ":" + cfg.Port
+
 	log.Print("Starting server")
-	err := http.ListenAndServe(":8080", router)
+	err = http.ListenAndServe(path, router)
 	if err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module prw_server
 
 go 1.17
 
-require github.com/go-chi/chi/v5 v5.0.7
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/ilyakaznacheev/cleanenv v1.2.5
+)
+
+require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+	olympos.io/encoding/edn v0.0.0-20200308123125-93e3b8dd0e24 // indirect
+)
